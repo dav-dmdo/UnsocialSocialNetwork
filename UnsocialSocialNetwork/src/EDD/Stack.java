e31d57f31dfb4428d851fa418ffff4e3e540936b
Andrea Linares <linares.andrea@correo.unimet.edu.ve>
2023-05-26 13:59:51 -0400

    :package: EDD package, Stack

--- UnsocialSocialNetwork/src/EDD/Stack.java
+++ UnsocialSocialNetwork/src/EDD/Stack.java
@@ -7,8 +7,79 @@ package EDD;
 
 /**
  *
- * @author Princ
+ * @author Andrea
  */
 public class Stack {
+    //Atributes
+    private Node top;
+    private Node base;
+    private int size;
+
+     /**
+     * CONSTRUCTORS 
+     */
+    
+    //EMPTY CONSTRUCTOR
+    public Stack() {
+        this.top = this.base = null;
+        this.size = 0;
+    }//END OF EMPTY CONSTRUCTOR 
+    
+    
+    public boolean isEmpty(){
+        return getTop() == null;
+    }
+
+    /**
+     * @return the top
+     */
+    public Node getTop() {
+        return top;
+    }
+
+    /**
+     * @param top the top to set
+     */
+    public void setTop(Node top) {
+        this.top = top;
+    }
+
+    /**
+     * @return the base
+     */
+    public Node getBase() {
+        return base;
+    }
+
+    /**
+     * @param base the base to set
+     */
+    public void setBase(Node base) {
+        this.base = base;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    /**
+     * GETTERS AND SETTERS
+     */
+    
+    
+    
+   
+    
+    
     
 }
