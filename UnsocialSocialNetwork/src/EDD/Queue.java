2f47fcf610d12a43ec0c768052b888fa122d3989
Andrea Linares <linares.andrea@correo.unimet.edu.ve>
2023-05-26 14:21:38 -0400

    :package: EDD fixed nodo, basics of stack and queue

---
+++ UnsocialSocialNetwork/src/EDD/Queue.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EDD;
+
+/**
+ *
+ * @author Andrea Linares
+ */
+public class Queue {
+    //ATRIBUTES
+    private Node head;
+    private Node tail;
+    private int size;
+
+    /**
+     * @return the head
+     */
+    public Node getHead() {
+        return head;
+    }
+
+    /**
+     * @param head the head to set
+     */
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    /**
+     * @return the tail
+     */
+    public Node getTail() {
+        return tail;
+    }
+
+    /**
+     * @param tail the tail to set
+     */
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    
+    
+}
--- UnsocialSocialNetwork/src/EDD/Stack.java
+++ UnsocialSocialNetwork/src/EDD/Stack.java
@@ -10,26 +10,88 @@ package EDD;
  * @author Andrea
  */
 public class Stack {
+
     //Atributes
     private Node top;
     private Node base;
     private int size;
 
-     /**
-     * CONSTRUCTORS 
+    /**
+     * CONSTRUCTORS
      */
-    
     //EMPTY CONSTRUCTOR
     public Stack() {
         this.top = this.base = null;
         this.size = 0;
     }//END OF EMPTY CONSTRUCTOR 
-    
-    
-    public boolean isEmpty(){
+
+    //METHODS
+    /**
+     * Checks if this stack is empty.
+     *
+     * @return true if this stack is empty and false otherwise.
+     */
+    public boolean isEmpty() {
         return getTop() == null;
     }
 
+    /**
+     * Empties a stack
+     */
+    public void empty() {
+        this.top = this.base = null;
+        this.size = 0;
+    }
+
+    /**
+     * Stacks in a pile receiving a Node
+     *
+     * @param nodo
+     */
+    public void stack(Node nodo) {
+        if (this.isEmpty()) {
+            top = base = nodo;
+        } else {
+            nodo.setpNext(top);
+            top = nodo;
+        }
+        size++;
+    }
+
+    /**
+     * Unstack the top element of the stack
+     */
+    public void unstack() {
+        if (!this.isEmpty()) {
+            if (size == 1) {
+                this.empty();
+            } else {
+                top = top.getpNext();
+                size--;
+            }
+        }
+    }
+
+    /**
+     * This is a recursive method that returns a string ready to be shown.
+     *
+     * @param printStack
+     * @return String
+     */
+    public String printList(String printStack) {
+        String toPrint = "";
+        if (!this.isEmpty()) {
+            Node aux = top;
+            this.unstack();
+            toPrint += aux.getData() + "-->";
+            toPrint = printList(toPrint);
+            this.stack(aux);
+
+        }
+        return toPrint;
+    }
+
+    //GETTERS AND SETTERS
     /**
      * @return the top
      */
@@ -71,15 +133,5 @@ public class Stack {
     public void setSize(int size) {
         this.size = size;
     }
-    
-    /**
-     * GETTERS AND SETTERS
-     */
-    
-    
-    
-   
-    
-    
-    
+
 }
