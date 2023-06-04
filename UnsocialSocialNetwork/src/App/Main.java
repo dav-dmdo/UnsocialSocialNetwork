e3032c821ea6e9baa03317c920349d7ebc952172
dav-dmdo <david.davila@correo.unimet.edu.ve>
2023-06-04 17:20:06 -0400

    DFS added

--- UnsocialSocialNetwork/src/App/Main.java
+++ UnsocialSocialNetwork/src/App/Main.java
@@ -28,10 +28,10 @@ public class Main {
         
 
 
-        App app = new App();
-        app.start();
-//
-//        GraphM grafo = new Graph();
+        //App app = new App();
+        //app.start();
+
+        GraphM grafo = new GraphM();
 //        User david = new User(1, "@dmdo");
 //        User moises = new User(2, "@dmdo");
 //        User davila = new User(3, "@dmdo");
@@ -55,70 +55,30 @@ public class Main {
 //        grafo.newEdge(friendship4);
 //        grafo.newEdge(friendship5);
 //        grafo.newEdge(friendship6);
-//        
-//        FileManager fileManager = new FileManager();
-//        
-//        fileManager.writeDBtxt(grafo);
-//        
-//        
-//        fileManager.fromJFCtoDB();
-//        
-//        grafo = fileManager.readDBtxt();
-//        
-//        System.out.println(grafo.toString());
-//        
-//        
-//        fileManager.writeDBtxt(grafo);
-//        
-//        
-//        Explorer explorer = new Explorer();
-//        boolean [] discovered = new boolean[grafo.getNumNodes()];
-//        int cant = explorer.DFS(grafo, grafo.getpFirst(), discovered);
-//        System.out.println("La cantidad de islas es:"+ cant);
-//
-//=======
-//        App app = new App();
-//        app.start();
-////
-//        GraphM grafo = new GraphM();
-//        User david = new User(1, "@dmdo");
-//        User moises = new User(2, "@dmdo");
-//        User davila = new User(3, "@dmdo");
-//        User ortiz = new User(4, "@dmdooo");
-//        
-//        grafo.newNode(david);
-//        grafo.newNode(moises);
-//        grafo.newNode(davila);
-//        grafo.newNode(ortiz);
-//        
-//        Friendship friendship1 = new Friendship(david.getUserID(), moises.getUserID(), 12);
-//        Friendship friendship2 = new Friendship(david.getUserID(), davila.getUserID(), 13);
-//        Friendship friendship3 = new Friendship(david.getUserID(), ortiz.getUserID(),14);
-//        Friendship friendship4 = new Friendship(moises.getUserID(), davila.getUserID(), 23);
-//        Friendship friendship5 = new Friendship(moises.getUserID(), ortiz.getUserID(), 24);
-//        Friendship friendship6 = new Friendship(davila.getUserID(), ortiz.getUserID(), 34);        
-//        
-//        grafo.newEdge(friendship1);
-//        grafo.newEdge(friendship2);
-//        grafo.newEdge(friendship3);
-//        grafo.newEdge(friendship4);
-//        grafo.newEdge(friendship5);
-//        grafo.newEdge(friendship6);
-//        
-//        FileManager fileManager = new FileManager();
-//        
-//        fileManager.writeDBtxt(grafo);
-//        
-//        
-//        fileManager.fromJFCtoDB();
-//        
-//        grafo = fileManager.readDBtxt();
-//        
-//        System.out.println(grafo.toString());
-//        
-//        
-//        fileManager.writeDBtxt(grafo);
-//        
+        
+        FileManager fileManager = new FileManager();
+        
+        //fileManager.writeDBtxt(grafo);
+        
+        
+        //fileManager.fromJFCtoDB();
+        
+        grafo = fileManager.readDBtxt();
+        
+        System.out.println(grafo.toString());
+        
+        
+        fileManager.writeDBtxt(grafo);
+        
+        
+        Explorer explorer = new Explorer();
+        //boolean [] visited = new boolean[grafo.getNumNodes()];
+        //explorer.DFS_programiz(grafo, grafo.getpFirst(), visited);
+        //System.out.println("La cantidad de islas es:"+ cant);
+        
+        explorer.completeDFS(grafo);
+
+
 
     }
     
--- UnsocialSocialNetwork/src/Grafo/Recorridos/Explorer.java
+++ UnsocialSocialNetwork/src/Grafo/Recorridos/Explorer.java
@@ -47,14 +47,61 @@ public class Explorer {
         }
         return cantIslands;
     }
+    
+    public void DFS2(GraphM graph, GraphNode root, boolean[] discovered, int islas){
+                
+        discovered [root.getIndex()] = true;                
+        Edge edge = root.getList().getpFirst();        
+        for (int i = 0; i < root.getList().getSize(); i++) {
+            if (!discovered[edge.getDestination().getIndex()]){             
+                DFS(graph, edge.getDestination(), discovered);                
+                System.out.println("---"+root.getUser().getUserID()+"---");        
+            }
+            edge = edge.getpNext();
+        }        
+    }
         
-    public int countIslands(GraphM graph, GraphNode root, boolean[] discovered ){
+    public int countIslands(GraphM graph, GraphNode root ){
         int cantIslands = 0;
+        boolean [] discovered = new boolean[graph.getNumNodes()];
         
-        
+        for (int i = 0; i < discovered.length; i++) {
+                DFS(graph, graph.searchByIndex(i), discovered);
+            }           
+                
         return cantIslands;     
               
                
     }
     
+    
+    public void DFS_programiz(GraphM g, GraphNode n, boolean [] visited){
+        
+        visited[n.getIndex()] = true;
+        
+        Edge adj = n.getList().getpFirst();
+        for (int i = 0; i < n.getList().getSize(); i++) {
+            if (!visited[adj.getDestination().getIndex()]){
+                DFS_programiz(g, adj.getDestination(), visited);
+                System.out.println(n.getUser().getUserID());
+            }
+            adj = adj.getpNext();
+        }
+        
+        
+    }
+    
+    public void completeDFS(GraphM g){
+        boolean [] visited = new boolean [g.getNumNodes()];
+        GraphNode n = g.getpFirst();
+        
+        for (int i = 0; i < g.getNumNodes(); i++) {
+            if (!visited[n.getIndex()]){
+                DFS_programiz(g, n, visited);
+                System.out.println("---");
+            }
+            n = n.getpNext();
+            
+        }
+    }
 }
